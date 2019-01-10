d7cd30b3ba28d1fb0148e968c39b68725cda96cd
Aminnoorani <aminnoorani@hotmail.com>
2019-01-11 00:45:36 +0330

    Rabbits and Recurrence Relations

---
+++ FIB.py
@@ -0,0 +1,11 @@
+def fibonacci(n, k):
+  # Initial conditions
+  if n == 1:
+    return 1
+  if n < 1:
+    return 0
+  else:
+  # Recursive part
+    return ((fibonacci(n - 2, k) * k) + fibonacci(n - 1, k))
+
+print(fibonacci(5, 3))
