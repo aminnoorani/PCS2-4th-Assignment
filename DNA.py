08df2b5a9162abfde8e7bf98e19c90374fae26ee
Aminnoorani <aminnoorani@hotmail.com>
2019-01-11 00:23:44 +0330

    Counting DNA Nucleotides

---
+++ DNA.py
@@ -0,0 +1,12 @@
+string  = input("enter your DNA nucleotides: ")
+string = string.upper()
+
+a_s = len([x for x in string if x=="A"])
+
+c_s = len([x for x in string if x=="C"])
+
+g_s = len([x for x in string if x=="G"])
+
+t_s = len([x for x in string if x=="T"])
+
+print(a_s, c_s,  g_s, t_s)
