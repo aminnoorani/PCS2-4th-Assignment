f3ec15dbdf91bbc190a43880516c7571365f44fa
Aminnoorani <aminnoorani@hotmail.com>
2019-01-12 01:56:14 +0330

    Finding a Spliced Motif

--- CONS.py
+++ CONS.py
@@ -1,36 +1,59 @@
-#returns a consensus string and profile matrix
-def consensus(fasta_file):
-    #creates list of dna strings from file in fasta format
-    lines = list(open(fasta_file, "r"))
-    dna_strings, i = [], 0
-    while i < len(lines):
-        if lines[i][0] == ">":
-            j = i + 1
-            dna_string = ''
-            while j < len(lines) and lines[j][0] != ">":
-                dna_string = dna_string + lines[j].strip("\n")
-                j += 1
-            dna_strings.append(dna_string)
-        i += 1
-    #creates profile matrix
-    columns = len(dna_strings[0])
-    profiles = {'A': [0 for i in range(0, columns)], 'C': [0 for i in range(0, columns)],
-                'G' : [0 for i in range(0, columns)], 'T': [0 for i in range(0, columns)] }
-    for dna in dna_strings:
-        i = 0
-        while i < len(dna):
-            profiles[dna[i]][i] += 1
-            i += 1
-    #prints consensus string and profile matrix
-    cons_str = []
-    #zips all the values of profiles together to take max of each index
-    maxes = [max(i) for i in zip([max(i) for i in zip(profiles['A'], profiles['T'])], [max(i) for i in zip(profiles['G'], profiles['C'])])]
-    cons_str = maxes
-    for item in profiles.items():
-        i = 0
-        while i < len(maxes):
-            if maxes[i] == item[1][i]:
-                cons_str[i] = item[0]
-            i += 1
-        print(item[0] + ':', *item[1])
-    print(''.join(cons_str))
+def print_profile(nt, lst):
+    print(nt, ": ", sep = '', end = '')
+    for item in lst:
+        print(item, end = ' ')
+    print()
+
+f = open("rosalind_cons.txt")
+lst = f.read()
+f.close()
+
+lst = lst.replace('\n', '')
+lst = lst.split('>')
+dna = list()
+
+for item in lst:
+    if (len(item) < 1):
+        continue
+    v = item[13::]
+    dna.append(v)
+
+dnaLen = len(dna[0])
+profA = []
+profC = []
+profG = []
+profT = []
+for i in range(0, dnaLen):
+    profA.insert(i, 0)
+    profC.insert(i, 0)
+    profG.insert(i, 0)
+    profT.insert(i, 0)
+
+for i in range(0, dnaLen):
+    for j in range(0, len(dna)):
+        if (dna[j][i] == 'A'):
+            profA[i] += 1
+        elif (dna[j][i] == 'C'):
+            profC[i] += 1
+        elif (dna[j][i] == 'G'):
+            profG[i] += 1
+        elif (dna[j][i] == 'T'):
+            profT[i] += 1
+
+cons = str()
+for i in range(0, dnaLen):
+    x = max([profA[i], profC[i], profG[i], profT[i]])
+    if (profA[i] == x):
+        cons += "A"
+    elif (profC[i] == x):
+        cons += "C"
+    elif (profG[i] == x):
+        cons += "G"
+    elif (profT[i] == x):
+        cons += "T"
+
+print(cons)
+print_profile("A", profA)
+print_profile("C", profC)
+print_profile("G", profG)
+print_profile("T", profT)
