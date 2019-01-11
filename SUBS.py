b3b84285cee6533a61a07cf07234e39f877199b7
Aminnoorani <aminnoorani@hotmail.com>
2019-01-12 01:25:51 +0330

    Finding a Spliced Motif

--- SUBS.py
+++ SUBS.py
@@ -1,22 +1,13 @@
-# Note: Rosalind uses 1-based numbering, which is accounted for
-#       in my solution.
+def find_motif(s,t):
+    positions = ''
+    for i in range(len(s)):
+        if s[i] == t[0]:
+            if s[i:i+len(t)] == t:
+                positions += str(i+1)+' '
+    return positions
 
-# Note: Instead of using the .find method, I wanted to implement this
-#       from scratch, which is why the code is less concise.
 
-def find_motif(string, substring):
-    j, k, indices = 0, 0, ''
-    while k < len(string):
-        if string[j:k+1] == substring:
-            indices = indices + str(j+1) + ' '
-            j += 1
-            k = j
-        elif len(string[j:k+1]) >= len(substring):
-            j += 1
-            k = j
-        elif string[j] == substring[0]:
-            k += 1
-        else:
-            j += 1
-            k += 1
-    print(indices)
+with open('rosalind_subs.txt','r') as file:
+    content = file.read()
+DNA, subDNA = content.splitlines()
+print(find_motif(DNA, subDNA))
