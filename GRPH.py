e041a0d1d1110634a07cf6f8410920c903b6f240
Aminnoorani <aminnoorani@hotmail.com>
2019-01-12 03:09:37 +0330

    Finding a Spliced Motif

---
+++ GRPH.py
@@ -0,0 +1,21 @@
+dataset = open('rosalind_grph.txt').read().strip().split()
+
+names = []
+for i in range(len(dataset)):
+    if dataset[i][0] == ">":
+        names.append(dataset[i][1:])
+        dataset[i] = "-"
+
+del dataset[0]
+ds = ''.join(dataset)
+dataset = ds.split('-')
+
+
+out = ""
+for i in range(len(dataset)):
+    for j in range(len(dataset)):
+        if dataset[i] != dataset[j]:
+            if dataset[i][-3:] == dataset[j][0:3]:
+                out += names[i]+" "+names[j]+"\n"
+
+print(out)
